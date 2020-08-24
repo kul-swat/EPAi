c718ed85a158e69d69759e47311e8c73e6505a30
Swati Bohidar <46372483+kul-swat@users.noreply.github.com>
2020-08-24 18:22:55 +0100

    Add files via upload

---
+++ session6.1.py
@@ -0,0 +1,12 @@
+a = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace']
+b = ['spades', 'clubs', 'hearts', 'diamonds']
+
+from itertools import repeat
+c = list(itertools.chain(*list(repeat(a,len(b)))))
+d = list(itertools.chain(*list(repeat(b,len(a)))))
+
+e = list(zip(c,d))
+output = list(map(lambda suits, values: zip(suits,values), e))
+for i in output:
+    print(tuple(i))
+
